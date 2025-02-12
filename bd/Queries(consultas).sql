7b6fd241f2edbde29863c68e359d11da980862eb
Claudia <claurodher2@gmail.com>
2025-02-12 13:23:30 +0100

    relational diagram and sql

--- bd/Queries(consultas).sql
+++ bd/Queries(consultas).sql
@@ -1,3 +1,7 @@
 SELECT * FROM Projects;
-
 SELECT * FROM Authors;
+
+SELECT Authors.name, Projects.name
+FROM Authors, Projects
+WHERE Projects.fk_author = Authors.idAuthor
+AND Authors.name = 'Idoia';
--- bd/Tables.sql
+++ bd/Tables.sql
@@ -16,6 +16,12 @@ name varchar(45) not null,
 job varchar(45) not null,
 photo text not null
 );
+-- Añadir la columna de la foreign key en la tabla
+ALTER TABLE Projects ADD COLUMN fk_author INT;
+
+-- Asignar a la foreign key el valor del id de la tabla de autoras
+ALTER TABLE Projects ADD FOREIGN KEY (fk_author)
+REFERENCES Authors (idAuthor);
 
 INSERT INTO Projects (name, slogan, repo, demo, technologies, description, image)
 VALUES ('El taller', 'ven a realizar tus piezas de cerámica', 'https://github.com/Clautropolis/promo-b-modulo-4-team-2.git', 'https://adalab.es/', 'react, html, css, js, vite, node', 'Leave hair everywhere fooled again thinking the dog likes me or make meme, make cute face dismember a mouse and then regurgitate parts of it on the family room floor or going to catch the red dot today going to catch the red dot today sitting in a box yet lay on arms while youre using the keyboard. Stand in front of the computer screen lick the curtain just to be annoying and meow meow you are my owner so here is a dead rat for grass smells good, munch, munch, chomp, chomp. I love cuddles hell is other people cat fur is the new black .', 'https://labois.com/wp-content/uploads/2022/05/ceramista-artesana-taller-handmade-1-scaled.jpg' ),
@@ -29,3 +35,5 @@ VALUES ('Mel', 'ceramista', 'https://st3.depositphotos.com/6462898/17953/i/600/d
 ('Gemma', 'emprendedora', 'https://imagenes.eltiempo.com/files/image_1200_535/files/fp/uploads/2024/03/19/65f9d492598ea.r_d.1079-279-5658.jpeg'),
 ('Idoia', 'investigadora', 'https://st3.depositphotos.com/10614052/32691/i/450/depositphotos_326919082-stock-photo-portrait-of-young-asian-woman.jpg');
 
+USE freedb_firstProject2025;
+DESCRIBE Projects;
