5c610c6db5bc3f436917d5f5626e3ef1a8d97996
IdoiaBE <1992ibe@gmail.com>
2025-02-12 13:46:34 +0100

    migration fixed

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,12 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/project-promo-B-module-3-team-2/",
+  base: "/promo-b-modulo-4-team-2/",
   server: {
     watch: {
       usePolling: true,
     },
     open: true,
   },
-  base: '/project-promo-B-module-3-team-2/',
 });
